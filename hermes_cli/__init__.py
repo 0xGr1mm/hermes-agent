7bc32b575fefd81b21d0a64154c46178e03a7d3f
ethernet <arilotter@gmail.com>
2026-09-21 13:09:52 -0400

    chore: bump version to v0.22.2 (2026.9.21)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-__version__ = "0.22.1"
+__version__ = "0.22.2"
 __release_date__ = "2026.9.21"
 
 
@@ -43,4 +43,4 @@ def _ensure_utf8():
 
 
 _ensure_utf8()
-__release_rev_count__ = 40641
+__release_rev_count__ = 40642
