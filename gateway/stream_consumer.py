Carrie111998/Hermes

fix(stream_consumer): guard message_len_fn_for_chat against hot-reload AttributeError
When git pull updates the code while the gateway is running, a lazy
import of stream_consumer.py (which happens on the first message) can
pick up a NEW version of the module while the adapter classes in memory
(base.py) are still the OLD version. This causes an AttributeError when
the new stream_consumer.py calls methods that didn't exist in the old
adapter.

The call at stream_consumer.py:683-687 used a bare ternary expression
without error handling. Two other call sites in the same file and
in run.py already had try/except Exception guards.

Fix: replace the ternary with an if/else block wrapped in try/except
Exception, consistent with the pattern at line 1368-1373.

Fixes #72628

gateway/stream_consumer.py
@@ -680,11 +680,16 @@ class GatewayStreamConsumer:
         # Telegram 4096); native adapters return their scalar unchanged.
         # Gate on isinstance(BasePlatformAdapter) so test MagicMocks (whose
         # auto-attributes return mock objects, not callables) fall back to len.
-        _len_fn: "Callable[[str], int]" = (
-            self.adapter.message_len_fn_for_chat(self.chat_id)
-            if isinstance(self.adapter, _BasePlatformAdapter)
-            else len
-        )
+        # Guard the call with except Exception so a hot-reload (git pull while
+        # the gateway runs) does not crash when the lazy-imported stream_consumer
+        # calls methods the in-memory adapter doesn't have yet (#72628).
+        if isinstance(self.adapter, _BasePlatformAdapter):
+            try:
+                _len_fn = self.adapter.message_len_fn_for_chat(self.chat_id)
+            except Exception:
+                _len_fn = len
+        else:
+            _len_fn = len
         # Rich-capable adapters (Telegram rich messages) raise this above the
         # legacy per-message limit so a reply that fits one rich send/draft
         # isn't fragmented at 4096 while streaming.  See _raw_message_limit.

0xGr1mm/hermes-agent

(cherry picked from commit 84a70ccaf322c8aaa764e79b767293168ce2014e)

gateway/stream_consumer.py
@@ -621,8 +621,15 @@ class GatewayStreamConsumer(StreamTransportMixin, StreamFallbackMixin, StreamThi
     def _resolve_length_budget(self) -> "tuple[Callable[[str], int], int]":
         """Per-chat length function (relay adapters differ per chat, e.g. utf16) + budget.
         isinstance gate: MagicMock auto-attributes aren't callables; test doubles use len."""
-        len_fn = (self.adapter.message_len_fn_for_chat(self.chat_id)
-                  if isinstance(self.adapter, _BasePlatformAdapter) else len)
+        len_fn: "Callable[[str], int]" = len
+        if isinstance(self.adapter, _BasePlatformAdapter):
+            # Guarded: the consumer is lazy-imported per turn, so a git pull while
+            # the gateway runs can pair new consumer code with an old in-memory
+            # adapter lacking message_len_fn_for_chat (#72628).
+            try:
+                len_fn = self.adapter.message_len_fn_for_chat(self.chat_id)
+            except Exception:
+                len_fn = len
         return len_fn, max(500, self._raw_message_limit() - len_fn(self.cfg.cursor) - 100)
 
     async def _start_transports(self) -> None: